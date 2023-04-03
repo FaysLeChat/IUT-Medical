878d3f8489f8f8b2769de8f39f19f0f647d7f1ba
Lucie <fayslechat@gmail.com>
2023-04-03 16:26:11 +0200

    ⚡️ Favicon + Ajout d'icons

--- front/src/components/NavbarComponent.js
+++ front/src/components/NavbarComponent.js
@@ -5,24 +5,26 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Link} from "react-router-dom";
 import {Dropdown} from "react-bootstrap";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCalendar, faEnvelope, faHome, faUser} from "@fortawesome/free-solid-svg-icons";
 
 function NavbarComponent() {
     return (
         <>
             <Navbar bg="dark" variant="dark" expand="lg">
                 <Container>
-                    <Navbar.Brand href="#home">FullMedical Alchemist</Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/">FullMedical Alchemist</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link as={Link} to="/">Accueil</Nav.Link>
-                            <Nav.Link href="#about">Rendez-vous</Nav.Link>
-                            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+                            <Nav.Link as={Link} to="/"><FontAwesomeIcon icon={faHome} /> Accueil</Nav.Link>
+                            <Nav.Link as={Link} to="/rdv"><FontAwesomeIcon icon={faCalendar} /> Rendez-vous</Nav.Link>
+                            <Nav.Link as={Link} to="/contact"><FontAwesomeIcon icon={faEnvelope} /> Contact</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                     <Nav>
                         <Dropdown as={Nav.Item}>
-                            <Dropdown.Toggle as={Nav.Link}>Compte</Dropdown.Toggle>
+                            <Dropdown.Toggle as={Nav.Link}><FontAwesomeIcon icon={faUser} /> Compte</Dropdown.Toggle>
                             <Dropdown.Menu align="end">
                                 <Dropdown.Item as={Link} to="/login">Login</Dropdown.Item>
                                 <Dropdown.Item as={Link} to="/register">Register</Dropdown.Item>
