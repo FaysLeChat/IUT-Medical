60a3a9cab37ec3a03d33a000ebac12dc88e356e5
Lucie <fayslechat@gmail.com>
2023-04-03 15:58:40 +0200

    ⚡️ Résolution des routes & template + Navbar ajout

--- front/src/components/NavbarComponent.js
+++ front/src/components/NavbarComponent.js
@@ -1,10 +1,10 @@
-
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Link} from "react-router-dom";
+import {Dropdown} from "react-bootstrap";
 
 function NavbarComponent() {
     return (
@@ -20,6 +20,15 @@ function NavbarComponent() {
                             <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
+                    <Nav>
+                        <Dropdown as={Nav.Item}>
+                            <Dropdown.Toggle as={Nav.Link}>Compte</Dropdown.Toggle>
+                            <Dropdown.Menu align="end">
+                                <Dropdown.Item as={Link} to="/login">Login</Dropdown.Item>
+                                <Dropdown.Item as={Link} to="/register">Register</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Nav>
                 </Container>
             </Navbar>
         </>
