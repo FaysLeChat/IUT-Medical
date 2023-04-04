947f36d041ce4b9cfd0ebb92ffb748a9dc6b892b
Lucie <lucie62c@gmail.com>
2023-04-04 10:15:01 +0200

    ⚡️ Fix template

--- front/src/pages/Appointment.js
+++ front/src/pages/Appointment.js
@@ -1,16 +1,11 @@
 import React from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
-import NavbarComponent from "../components/NavbarComponent";
 import {Container} from "react-bootstrap";
 
 export default function Appointment() {
         return (
             <div className="App">
-                <header>
-                    <NavbarComponent />
-                </header>
-
                 <main>
                     <Container className="mt-5">
                         <FullCalendar plugins={[ dayGridPlugin ]} initialView="dayGridMonth" />
