631b581a87235ec72544e5f7735798becef6ab04
FaysLeChat <fayslechat@gmail.com>
2023-04-04 14:13:38 +0200

    ⚡️ Fix

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import AppRoutes from './routes';
+import {useCookies} from "react-cookie";
 
 const App = () => {
+    const [cookies, setCookie, removeCookie] = useCookies(["amigo"]);
     return (
         <div className="App">
-            <AppRoutes />
+            <AppRoutes cookies={cookies} setCookie={setCookie} removeCookie={removeCookie} />
         </div>
     );
 };
