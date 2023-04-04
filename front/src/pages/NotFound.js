efa506679ee79d2b1cdf6efab35dd52e5b8849ac
FaysLeChat <fayslechat@gmail.com>
2023-04-04 10:15:01 +0200

    ⚡️ Fix template

--- front/src/pages/NotFound.js
+++ front/src/pages/NotFound.js
@@ -2,15 +2,10 @@ import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css';
-import NavbarComponent from "../components/NavbarComponent";
 
 const NotFound = () => {
     return (
         <div className="App">
-            <header>
-                <NavbarComponent />
-            </header>
-
             <main>
                 <div className="error404">
                     <h1>Erreur 404</h1>
