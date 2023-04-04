4cae21d280d5f2c7839fab24b10bd09002333244
Allan Ponchaut <allanponchaut2@gmail.com>
2023-04-04 11:10:47 +0200

    installation de react-cookie + commencement implémentation cookie

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import AppRoutes from './routes';
+import {useCookies} from "react-cookie";
 
 const App = () => {
+    const [cookies, setCookie, removeCookie] = useCookies(["fullmedicalalchemist"]);
+
     return (
         <div className="App">
             <AppRoutes />
