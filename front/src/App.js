ad0300174a21742a674dd1b4f5970ec8f467b4e2
FaysLeChat <fayslechat@gmail.com>
2023-04-04 10:15:01 +0200

    ⚡️ Fix template

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import AppRoutes from './routes';
-import FooterComponent from "./components/FooterComponent";
 
 const App = () => {
     return (
         <div className="App">
             <AppRoutes />
-            <FooterComponent />
         </div>
     );
 };
